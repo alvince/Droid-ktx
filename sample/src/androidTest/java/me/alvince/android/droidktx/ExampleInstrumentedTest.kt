e05830497a81471187a29d3828e43ce330f10833
alvince <alvince.zy@gmail.com>
2018-12-12 15:49:30 +0800

    Add samples

---
+++ sample/src/androidTest/java/me/alvince/android/droidktx/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package me.alvince.android.droidktx
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("me.alvince.android.droidktx", appContext.packageName)
+    }
+}
---
+++ sample/src/main/java/me/alvince/android/droidktx/SampleActivity.kt
@@ -0,0 +1,17 @@
+package me.alvince.android.droidktx
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.alvince.android.droidktx.core.string
+
+/**
+ * Created by alvince on 2018/12/12
+ *
+ * @author alvince.zy@gmail.com
+ */
+class SampleActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
